cc3a0f64588dd71e65300b0ce54609ddab742af7
BillyChilly808 <147418914+BillyChilly808@users.noreply.github.com>
2025-05-31 10:28:16 -1000

    add auth middleware

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { getCurrentUser, updateUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
+
+router.use(auth);
 
 router.get("/me", getCurrentUser);
-
 router.patch("/me", updateUser);
 
 module.exports = router;
