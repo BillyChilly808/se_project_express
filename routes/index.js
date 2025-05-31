68d03eb7d23d283336b31d93f9e29a95d401de84
BillyChilly808 <147418914+BillyChilly808@users.noreply.github.com>
2025-05-31 10:29:47 -1000

    update index

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ const router = require("express").Router();
 const { NOT_FOUND } = require("../utils/constants");
 
 const userRouter = require("./users");
-const clothingItem = require("./clothingItem");
+const clothingItemRouter = require("./clothingItem");
+const auth = require("../middlewares/auth");
 const { createUser, login } = require("../controllers/users");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
 
-router.use("/items", clothingItem);
+router.get("/items", clothingItemRouter);
+
+router.use("/items", auth, clothingItemRouter);
 router.use("/users", userRouter);
 
 router.use((req, res) => {
